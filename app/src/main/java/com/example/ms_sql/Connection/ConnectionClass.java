3786a1ef52dabc45ec40d6f0ab51faccf77a5277
Jaimieyeh <jaimieyeh0706@gmail.com>
2020-12-01 13:30:54 +0800

    initial

---
+++ app/src/main/java/com/example/ms_sql/Connection/ConnectionClass.java
@@ -0,0 +1,8 @@
+package com.example.ms_sql.Connection;
+
+public class ConnectionClass {
+    public static String ip = "172.20.10.2";
+    public static String un = "sa";
+    public static String pass = "0000";
+    public static String db = "test";
+}
---
+++ app/src/main/java/com/example/ms_sql/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.ms_sql;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import java.sql.Connection;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
